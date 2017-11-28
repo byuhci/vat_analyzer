c28218182612c54137a343db34036b364580045a
naomi789 <snjohnson789@gmail.com>
2017-11-28 14:10:03 -0700

    changing input style for survey script

--- surveycheck.py
+++ surveycheck.py
@@ -1,3 +1,6 @@
+import json
+from sys import argv
+
 user_info = {}
 run_blue = {}
 run_yellow = {}
@@ -11,33 +14,54 @@ pill_pink = {}
 pill_survey = {}
 overall_survey  = {}
 
-SURVEY_PATH = "/home/austin/workspacegold"
+both = {}
+noVideo = {}
+noData = {}
+
+output_file = argv[1]
+
+SURVEY_PATH = "/home/naomi/Documents/AML/vat/data/Tues9PM/"
+
+#NEED TO KEEP TRACK OF WHEN/WHAT IS STUDY A B C OR D
 
 def findSurveyFiles(theWorkSpace, time):
-    for root, dirs, files in os.walk(SURVEY_PATH):
+    for dirpath, dirs, files in os.walk(SURVEY_PATH):
         for file in files:
-            filename = os.path.join(root,file)
-            survey_filename = json.load(data_file)
-            split_survey_filename = filename.split('/')
-            final_split = split_survey_filename[-1].split('.')
+            filename = os.path.join(dirpath,file)
+            survey_dictionary = json.load(filename)
+            split_survey_dictionary = filename.split('/')
+            final_split = split_survey_dictionary[-1].split('.')
             with open(filename) as data_file:
-            # print("final_split[0]")
-            # print(final_split[0])
+            print("final_split[0]")
+            print(final_split[0])
             	if(final_split[0] in theWorkSpace):
-                	return findSurveyAnswers(survey_filename)
-    return "No surveys in hardcoded directory"
+                	yield findSurveyAnswers(survey_dictionary)
 
-def findSurveyAnswers(survey_filename):
-    for surveys in survey_filename:
+def findSurveyAnswers(survey_dictionary):
+	for survey in filter(lambda x: x['study'] == 'studyA', findSurveyFiles()); 
+	for survey in filter(lambda x: x['study'] == 'studyB', findSurveyFiles()); 
+	for survey in filter(lambda x: x['study'] == 'studyC', findSurveyFiles()); 
+	for survey in filter(lambda x: x['study'] == 'studyD', findSurveyFiles()); 
+
+
+
+
+    if 'surveys' in info:
+    	#for 
         #print("in findSurveyAnswers at loop")
         #for #each survey in this survey #make an obj/array in the appropriate place
         	#for each question in this survey #save the answer in the right obj/array (note that scale might beed to change? )
         	#save the 
-    	return "cool error message because the files didn't have surveys?!"
 
 def findSurveyAverages():
 	#for each question that we have data
 		#how many people answered that question, what the average was, what the outliers were
 		#maybe we just want to return all the data so we can visualize it?? 
 		#do I care enough to synchronize the data with the questions? probably not? 
-	return "not doing anything here yet"
+
+
+
+
+
+#things I might need later
+#import pdb; pdb.set_trace()
