e471659ab660323e7e2dd9e96b0281d361dbfac2
Austin Gardner <austingardner66@gmail.com>
2017-12-08 17:23:58 -0700

    fscores almost done

--- verifyMatrix.py
+++ verifyMatrix.py
@@ -1,5 +1,5 @@
 import json
-import ankura
+#import ankura
 import numpy as np
 import copy
 
@@ -27,7 +27,11 @@ def putInNumpyMatrix(perPersonConfusionMatrices):
                         val = row.get(kk,0)
                         matrix[i][j] = val
             elif workspace in cane_andor_practice_workspaces:
-                continue
+                for i, k in enumerate(['None', 'ground']):
+                    row = c[k]
+                    for j, kk in enumerate(['None', 'ground']):
+                        val = row.get(kk,0)
+                        matrix[i][j] = val
             elif workspace in pill_workspaces:
                 for i, k in enumerate(['None', 'move', 'pill']):
                     row = c[k]
@@ -54,36 +58,38 @@ def createConfusion(perPersonConfusionMatrices, person, workspace):
 def getFFromConfusion(c):
     #note: recall row, axis=1 is row. precision column, axis=0 is column
     print("c.diagonal" + str(c.diagonal()) + " c.sum(axis=1) " + str(c.sum(axis=1)) + " c.sum(axis=0) " + str(c.sum(axis=0)))
-    recall = c.diagonal()/c.sum(axis=1)
+    #will both be 1 x 3 matrices
+    # recall = c.diagonal()/c.sum(axis=1)
+    with np.errstate(divide='ignore'):
+        recall = c.diagonal() / c.sum(axis=1)
+        recall[np.isnan(recall)] = 0
+        precision = c.diagonal()/c.sum(axis=0)
+        precision[np.isnan(precision)] = 0
+    # recall = np.divide(c.diagonal(), c.sum(axis=1), out=np.zeros_like(c.diagonal()), where=c.sum(axis=1)!=0)
     #print(recall)
-    precision = c.diagonal()/c.sum(axis=0)
+    #precision = c.diagonal()/c.sum(axis=0)
+    # precision = np.divide(c.diagonal(), c.sum(axis=0), out=np.zeros_like(c.diagonal()), where=c.sum(axis=0)!=0)
+    #np.divide(a, b, out=np.zeros_like(a), where=b!=0)
     #print(precision)
-    return 2 * ((recall * precision) / (recall + precision))
+    ave_recall = sum(recall) / len(recall)
+    ave_precision = sum(precision) / len(precision)
+    return 2 * ((ave_recall * ave_precision) / (ave_recall + ave_precision))
 
-# def get_f_score_per_Person_confusion(perPersonConfusionMatrices):
-#     fscores = {}
-#     for person in perPersonConfusionMatrices:
-#         for workspace in perPersonConfusionMatrices[person]:
-#             c = ankura.validate.Contingency()
-#             for gold in perPersonConfusionMatrices[person][workspace]:
-#                 for user in perPersonConfusionMatrices[person][workspace][gold]:
-#                     c[gold,user] = perPersonConfusionMatrices[person][workspace][gold][user]
-#                     print("c[gold][user]")
-#                     print(c[gold,user])
-#             #it works up to here, then breaks at the fmeasure
-#             print("c.self.table")
-#             print(c.table)
-#             score = c.fmeasure(None, None)
-#             print("score")
-#             print(score)
-#             if workspace in fscores:
-#                 fscores[workspace][1] += score
-#                 fscores[workspace][0] += 1
-#             else:
-#                 fscores[workspace] = [1, score]
-#
-#
-#     return fscores
+def getAveragePerPersonFScore(fscores):
+    f_list = []
+    workspacePerPersonAve = {}
+    for person in fscores:
+        for workspace in fscores[person]:
+            f_list.append(fscores[person][workspace])
+            if workspace in workspacePerPersonAve:
+                workspacePerPersonAve[workspace].append(fscores[person][workspace])
+            else:
+                workspacePerPersonAve[workspace] = [fscores[person][workspace]]
+    return sum(f_list) / len(f_list), workspacePerPersonAve
+
+def getAverageWorkspacePerPerson(wppa):
+    for workspace in wppa:
+        wppa[workspace] = sum(wppa[workspace]) / len(wppa[workspace])
 
 def get_f_score_per_Person_confusion(perPersonConfusionMatrices):
     fscores = {}
@@ -118,102 +124,19 @@ def get_f_score_per_Person_confusion(perPersonConfusionMatrices):
     return fscores
 
 
-def getfscore(c):
-    print("in getfscore")
-    recall = getRecall(c)
-    precision = getPrecision(c)
-    return 2 * ((recall * precision) / (recall + precision))
-
-def getRecall(c):
-    print("in getRecall")
-    my_recalls = []
-    for gold in c:
-        TP = -1
-        my_sum = []
-        for user in c[gold]:
-            my_sum.append(c[gold][user])
-            if user == gold:
-                TP = c[gold][user]
-        one_recall = TP / sum(my_sum)
-        my_recalls.append(one_recall)
-    final_recall = float(sum(my_recalls)) / max(len(my_recalls), 1)
-    return final_recall
-
-def getPrecision(c):
-    print("in getPrecision")
-    prec_list = []
-    max_size_list = []
-    gold_size = 0
-    for gold in c:
-        gold_size+=1
-        size = 0
-        for user in c[gold]:
-            size+=1
-        max_size_list.append(size)
-        max_size_list.append(gold_size)
-    max_size_num = max(max_size_list)
-
-    zeros = [[0 for i in range(max_size_num)] for j in range(max_size_num)]
-    print("c")
-    print(c)
-    print("max_size_list")
-    print(max_size_list)
-    print("zeros pre ")
-    print(zeros)
-
-
-    gold_index = 0
-    tp_list = []
-    for gold in c:
-        user_index = 0
-        for user in c[gold]:
-            print("gold_index")
-            print(gold_index)
-            print("user_index")
-            print(user_index)
-            zeros[gold_index][user_index] = c[gold][user]
-            if gold == user:
-                tp_list.append((gold_index,user_index))
-            user_index+=1
-        gold_index+=1
-
-    print("zeros")
-    print(zeros)
-
-    my_precisions = []
-    for j in range(max_size_num):
-        TP = -1
-        sum_list = []
-        for i in range(max_size_num):
-            sum_list.append(zeros[i][j])
-            if (i,j) in tp_list:
-                TP = zeros[i][j]
-        one_precision = TP / sum(sum_list)
-        my_precisions.append(one_precision)
-    final_precision = float(sum(my_precisions)) / max(len(my_precisions), 1)
-    return final_precision
-
 #print("fscores\n " + get_f_score_per_Person_confusion(myFile))
 np_arrs, f_scores = putInNumpyMatrix(myFile)
-print("np_arrs")
+print("np_arrs\n\n\n")
 print(np_arrs)
-print("f_scores")
+print("f_scores\n\n\n")
 print(f_scores)
-# print(f_scores)
-# theScores = get_f_score_per_Person_confusion(myFile)
-# print(theScores)
+print("\n\n\naverage per person fscore")
+all_ave, wppa = getAveragePerPersonFScore(f_scores)
+getAverageWorkspacePerPerson(wppa)
+print(all_ave)
+print("\n\n\naverages per workspace per person")
+print(wppa)
 
-# for person in myFile:
-#     print "this is person"
-#     print person
-#     for workspace in myFile[person]:
-#         print "this is workspace"
-#         print workspace
-#         for gold in myFile[person][workspace]:
-#             print "this is gold"
-#             print gold
-#     #         break
-#     #     break
-#     # break
+
 
 #I NEED to know two things: best way to integrate functions of Ankura from github, AND how to put this matrix into those functios
