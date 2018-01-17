106ed9a77f0a15e857b7cd0838dca6d5d3bf8ebe
naomi789 <snjohnson789@gmail.com>
2018-01-16 20:50:21 -0700

    coding with Alyssa

--- matplot.py
+++ matplot.py
@@ -1,30 +1,58 @@
-import numpy as np
+# # import numpy as np
+# import matplotlib.pyplot as plt
+# import csv
+# import itertools
+#
+#
+# x = []
+# y = []
+#
+# title = [] # String of question
+# point = [] # data point
+# line = [] # vid/dat hidden
+# allGraphs = {}
+# listOfTuples = []
+# translation = {}
+#
+# with open('surveyResults.csv', 'r') as csvfile:
+#     plots = csv.reader(csvfile, delimiter=',')
+#     for row in plots:
+#         translation = {'not applicable': 0, 'no-data-task':1, 'no-video-task':2,'has-both-task':3 }
+#
+#         if row[5] not in allGraphs:
+#             allGraphs[row[5]] = [int(row[2]), yvalue]
+#         else:
+#             allGraphs[row[5]].append(int(row[2]))
+#             allGraphs[row[5]].append(yvalue)
+#
+#
+# for item in allGraphs:
+#     manyPairs = allGraphs[item]
+#     for dataPoint in manyPairs:
+#         print("hi")
+#         # how can I access dataPoint and dataPoint+1??
+#         # plt.plot(pair[1], pair[0], label=item)
+#         # plt.xlabel('what was visible')
+#         # plt.ylabel('satisfaction/success')
+#         # plt.title(item)
+#         # plt.legend()
+#         # plt.show()
+#
+#
+import csv
+import itertools
 import matplotlib.pyplot as plt
 
 
-N = 50
-x = np.random.rand(N)
-y = np.random.rand(N)
-colors = np.random.rand(N)
-area = np.pi * (15 * np.random.rand(N))**2  # 0 to 15 point radii
+with open('surveyResults.csv') as f:
+    data = list(csv.reader(f, delimiter=','))
+    data.sort(key=lambda row: row[5])
 
-plt.scatter(x, y, s=area, c=colors, alpha=0.5)
-plt.show()
-
-X = np.linspace(-np.pi, np.pi, 256,endpoint=True)
-C,S = np.cos(X), np.sin(X)
-
-
-plt.plot(X, C, color="blue", linewidth=2.5, linestyle="-")
-plt.plot(X, S, color="red", linewidth=2.5, linestyle="-")
-
-
-plt.xlim(X.min()*1.1, X.max()*1.1)
-plt.xticks([-np.pi, -np.pi/2, 0, np.pi/2, np.pi],
-[r'$-\pi$', r'$-\pi/2$', r'$0$', r'$+\pi/2$', r'$+\pi$'])
-
-plt.ylim(C.min()*1.1,C.max()*1.1)
-plt.yticks([-1, 0, +1],
-[r'$-1$', r'$0$', r'$+1$'])
-
-plt.show()
+for group_name, rows in itertools.groupby(data, lambda row: row[5]):
+    plt.title(group_name)
+    rows = sorted(list(rows), key=lambda row: row[4])
+    xs, heights, names = zip(*((n, int(row[2]), row[4]) for n, row in enumerate(rows)))
+    print(group_name, xs, names, heights)
+    plt.xticks(xs, names)
+    plt.bar(xs, heights)
+    plt.show()
