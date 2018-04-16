49d304901faf5a20f20379afbcffd81ad8575951
naomi789 <snjohnson789@gmail.com>
2018-04-15 23:07:08 -0600

    running script for figure 4: objective correct missed/stacked efficiency charts, only mann-whitney (wilcoxon errors)

---
+++ post_writing_paper_scripts/wilcoxon_friedman_scripts.py
@@ -0,0 +1,41 @@
+import csv
+import scipy.stats
+from scipy.stats import mannwhitneyu
+from time import strftime
+
+
+# stuff I found in subjectiveAnswer.py
+
+def wilcoxonTest(hasData, hasVideo):  # non parametric test
+    print(strftime("%Y-%m-%d %H:%M"))
+    f = open('wilcoxon_' + strftime("%Y-%m-%d %H:%M") + '.txt', 'w')
+    f.write('\n')
+    f.write(str(mannwhitneyu(hasData, hasVideo)))
+    # f.write(str(scipy.stats.wilcoxon(hasData, hasVideo)))
+    f.close()
+
+
+def friedmanLikertTest(analyzableData):
+    print(strftime("%Y-%m-%d %H:%M"))
+
+
+def readInData():
+    hasVideo = []
+    hasData = []
+    # read in percentComplete.csv # for percent complete, paired with hidden value
+    with open('percentComplete.csv', 'r') as csvfile:
+        allRows = csv.reader(csvfile, delimiter=',')  # , quotechar='|'
+        for row in allRows:
+            if row[1] == 'data':  # data is hidden
+                # store in one place
+                hasVideo.append(row[0])
+            elif row[1] == 'video':  # video is hidden
+                # store in other
+                hasData.append(row[0])
+            # print(row[0], row[1])
+    return hasData, hasVideo
+
+
+hasData, hasVideo = readInData()
+wilcoxonTest(hasData, hasVideo)
+print('finished')
