4863f5b8dc774a41d993369461960fe74a8bb46b
naomi789 <snjohnson789@gmail.com>
2018-04-15 23:39:23 -0600

    formatting code

--- post_writing_paper_scripts/wilcoxon_friedman_scripts.py
+++ post_writing_paper_scripts/wilcoxon_friedman_scripts.py
@@ -10,8 +10,8 @@ def wilcoxonTest(hasData, hasVideo):  # non parametric test
     print(strftime("%Y-%m-%d %H:%M"))
     f = open('wilcoxon_' + strftime("%Y-%m-%d %H:%M") + '.txt', 'w')
     f.write('\n')
-    f.write(str(mannwhitneyu(hasData, hasVideo)))
-    # f.write(str(scipy.stats.wilcoxon(hasData, hasVideo)))
+    # f.write(str(mannwhitneyu(hasData, hasVideo)))
+    f.write(str(scipy.stats.wilcoxon(hasData, hasVideo)))
     f.close()
 
 
@@ -32,10 +32,16 @@ def readInData():
             elif row[1] == 'video':  # video is hidden
                 # store in other
                 hasData.append(row[0])
+            elif row[1] == 'none':
+                # do nothing
+                1 + 1
+            else:
+                print(row[1])
             # print(row[0], row[1])
     return hasData, hasVideo
 
 
 hasData, hasVideo = readInData()
+print(len(hasVideo), len(hasData))
 wilcoxonTest(hasData, hasVideo)
 print('finished')
