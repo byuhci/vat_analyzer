bca4be2d1ceb81dac7bb6c755d6ffe57b5b1927b
naomi789 <snjohnson789@gmail.com>
2018-04-16 13:43:54 -0600

    moved data into folder, script outputs more data

--- post_writing_paper_scripts/mannWhitney_friedman_scripts.py
+++ post_writing_paper_scripts/mannWhitney_friedman_scripts.py
@@ -8,9 +8,12 @@ from time import strftime
 
 def mannWhitney(hasData, hasVideo):  # non parametric test
     print(strftime("%Y-%m-%d %H:%M"))
-    f = open('wilcoxon_' + strftime("%Y-%m-%d %H:%M") + '.txt', 'w')
-    f.write('\n')
-    f.write(str(mannwhitneyu(hasData, hasVideo)))
+    f = open('results_mann_whitney/wilcoxon_' + strftime("%Y-%m-%d %H:%M") + '.txt', 'w')
+    f.write('numToEnglish[whatLookingAt]: ' + numToEnglish[whatLookingAt] + '\n')
+    f.write('comparing hasData to hasVideo: ' + '\n')
+    # f.write("hasData: " + str(hasData))
+    # f.write("hasVideo:" + str(hasVideo))
+    f.write(str(mannwhitneyu(hasData, hasVideo)) + '\n')
     # f.write(str(scipy.stats.wilcoxon(hasData, hasVideo)))
     f.close()
 
@@ -32,15 +35,32 @@ def readInData():
         for row in allRows:
             if row[0] == 'data':  # data is hidden
                 # store in one place
-                hasVideo.append(row[0])
+                hasVideo.append(row[whatLookingAt])
             elif row[0] == 'video':  # video is hidden
                 # store in other
-                hasData.append(row[0])
+                hasData.append(row[whatLookingAt])
 
     return hasData, hasVideo
+
+# global vars = the best
+whatLookingAt = 1
+
+
+numToEnglish = {
+    0: 'hidden',
+    1: 'correct',
+    2: 'mislabeled',
+    3: 'off',
+    4: 'wrong',
+    5: 'missed',
+    6: 'mislabled AND off'
+}
+
 
 
 hasData, hasVideo = readInData()
 print(len(hasVideo), len(hasData))
+print("hasVideo", hasVideo)
+print("hasData", hasData)
 mannWhitney(hasData, hasVideo)
 print('finished')
