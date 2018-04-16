c20651fe361d39cd4b8ccc8b36b8cacae72eb1e6
naomi789 <snjohnson789@gmail.com>
2018-04-16 13:26:27 -0600

    new dataset, switching to mannwhitney

--- post_writing_paper_scripts/wilcoxon_friedman_scripts.py
+++ post_writing_paper_scripts/wilcoxon_friedman_scripts.py
@@ -6,12 +6,12 @@ from time import strftime
 
 # stuff I found in subjectiveAnswer.py
 
-def wilcoxonTest(hasData, hasVideo):  # non parametric test
+def mannWhitney(hasData, hasVideo):  # non parametric test
     print(strftime("%Y-%m-%d %H:%M"))
     f = open('wilcoxon_' + strftime("%Y-%m-%d %H:%M") + '.txt', 'w')
     f.write('\n')
-    # f.write(str(mannwhitneyu(hasData, hasVideo)))
-    f.write(str(scipy.stats.wilcoxon(hasData, hasVideo)))
+    f.write(str(mannwhitneyu(hasData, hasVideo)))
+    # f.write(str(scipy.stats.wilcoxon(hasData, hasVideo)))
     f.close()
 
 
@@ -20,28 +20,27 @@ def friedmanLikertTest(analyzableData):
 
 
 def readInData():
+    # hidden	% correct	% mislabelled	% off	% wrong	% missed
     hasVideo = []
     hasData = []
     # read in percentComplete.csv # for percent complete, paired with hidden value
-    with open('percentComplete.csv', 'r') as csvfile:
+    # format of various_percents_for_mann_whitney.csv
+    # hidden,correct,"not type, bounds","type, not bounds","not type, not bounds",missed,"not type, (bounds || not bounds)"
+    # hidden,% correct,% mislabelled,% off,% wrong,% missed,% mislabelled && off
+    with open('various_percents_for_mann_whitney.csv', 'r') as csvfile:
         allRows = csv.reader(csvfile, delimiter=',')  # , quotechar='|'
         for row in allRows:
-            if row[1] == 'data':  # data is hidden
+            if row[0] == 'data':  # data is hidden
                 # store in one place
                 hasVideo.append(row[0])
-            elif row[1] == 'video':  # video is hidden
+            elif row[0] == 'video':  # video is hidden
                 # store in other
                 hasData.append(row[0])
-            elif row[1] == 'none':
-                # do nothing
-                1 + 1
-            else:
-                print(row[1])
-            # print(row[0], row[1])
+
     return hasData, hasVideo
 
 
 hasData, hasVideo = readInData()
 print(len(hasVideo), len(hasData))
-wilcoxonTest(hasData, hasVideo)
+mannWhitney(hasData, hasVideo)
 print('finished')
